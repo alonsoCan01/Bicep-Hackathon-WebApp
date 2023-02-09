7b5fe75f3db390224654e84cdf74d193766fdd4c
DavidGianni3 <78496524+DavidGianni3@users.noreply.github.com>
2023-02-09 11:35:35 -0500

    Changed team names

--- src/backend/api/Team.cs
+++ src/backend/api/Team.cs
@@ -16,13 +16,13 @@ namespace api
 
         public Team()
         {
-            teamName = "Team Bicep Curls";
+            teamName = "Team very creative name";
 
             teamMembers = new List<TeamMember>();
 
-            teamMembers.Add(new TeamMember("Drake", "Do a lunch and learn"));
-            teamMembers.Add(new TeamMember("Drew", "Sounds good"));
-            teamMembers.Add(new TeamMember("Nasir", "Let me know if you need help"));
+            teamMembers.Add(new TeamMember("David", "I'm going to be a few minutes late to the meeting"));
+            teamMembers.Add(new TeamMember("Alonso", "Thats fine im setting up"));
+            teamMembers.Add(new TeamMember("Colin", "If you aint first your last"));
 
         }
 
